e6514b764afbce5f56ac454f94a3538dbd4e3c66
Daniel Habenicht <daniel-habenicht@outlook.de>
2021-06-11 01:32:08 +0200

    fix plz questions

--- src/components/views/questionnaire/utils.ts
+++ src/components/views/questionnaire/utils.ts
@@ -1,28 +1,28 @@
-// TODO: Use built in i18n Functions to translate
+import i18next from '../../../global/utils/i18n';
+
 export const QUESTION_SHARE_DATA = {
   options: [
     {
       scores: {},
-      text: 'Yes, I would like to donate my postal code and my recommended action',
+      text: i18next.t(`q_X1_option0`),
       value: 'yes',
     },
     {
       scores: {},
       value: 'no',
-      text:
-        'No, I do not want to transfer my data and I only want to see my recommended action',
+      text: i18next.t(`q_X1_option1`),
     },
   ],
   id: 'covapp_data_donation',
-  text: 'Consent to the transmission of your postal code and recommended action',
+  text: i18next.t(`q_X1_text`),
   type: 'select',
-  details:
-    'In order to draw valuable conclusions about the further expansion of the COVID-19 pandemic, we ask for your consent to transmit your postal code and your recommendation to Charité – Universitätsmedizin Berlin. The data will be stored in an anonymous form (postal code and recommendation) and if necessary, the data will be shared with third parties as part of the fight against pandemics. You can revoke your consent at any time. The revocation does not affect the lawfulness of processing based on consent before the revocation.\nIf you would like to give your consent to the transmission of the postal code and the recommendation of your recommended action, choose “Yes” below. You will then be asked for your postal code in the next step.  To continue without submitting your data, choose “No” below. You will then be forwarded directly to your summary.  You can find more information in the <a href="/data-privacy">privacy policy</a>.',
+  details: i18next.t(`q_X1_comment`),
 };
 export const QUESTION_SHARE_DATA_PLZ = {
   id: 'covapp_plz',
-  text: 'Ihre Postleitzahl',
+  text: i18next.t(`q_V1_text`),
   type: 'text',
+  details: i18next.t(`q_V1_comment`),
   enableWhenExpression: {
     var: `${QUESTION_SHARE_DATA.id}.option.yes`,
   },
