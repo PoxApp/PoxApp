f873fbe3b81430bed412f574614a93bfe540a236
alexanderthieme <61984544+alexanderthieme@users.noreply.github.com>
2022-02-11 12:40:52 -0800

    fix: Summary (#88)

--- src/components/views/summary/summary.tsx
+++ src/components/views/summary/summary.tsx
@@ -103,6 +103,17 @@ export class Summary {
                 </div>
               </ia-accordion> : undefined
               }
+              {
+                result.length > 1 ? 
+                <ia-accordion
+                headline={result[1].resultCategory.description}
+                open={false}
+                >
+                <div slot="accordion-children">
+                  <div innerHTML={DOMPurify.sanitize(result[1].result.text)} />
+                </div>
+              </ia-accordion> : undefined
+              }
               {result.slice(1).map(r => (
                 <ia-accordion headline={r.resultCategory.description} open={true}>
                   <div slot="accordion-children">
