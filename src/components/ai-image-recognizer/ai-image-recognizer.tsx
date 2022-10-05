6eda131fae92d784d10d3567d4c60d300f80bda0
DanielHabenicht <daniel-habenicht@outlook.de>
2022-10-05 15:31:13 +0000

    move encryption code to dataDonation Service

--- src/components/ai-image-recognizer/ai-image-recognizer.tsx
+++ src/components/ai-image-recognizer/ai-image-recognizer.tsx
@@ -10,7 +10,6 @@ import * as tf from '@tensorflow/tfjs';
 //import * as tfnode from '@tensorflow/tfjs-node';
 
 import i18next from 'i18next';
-import * as forge from 'node-forge';
 
 @Component({
   tag: 'ai-image-recognizer',
@@ -55,51 +54,7 @@ export class AiImageRecognizer {
   disconnectedCallback(){
   }
 
-  encrypt = async() =>  {
-    var someBytes = 'hello world!';
-
-    
-    var keypair = forge.pki.rsa.generateKeyPair(4096);
-    
-    
-    // generate and encapsulate a 16-byte secret key
-    var kdf1 = new forge.kem.kdf1(forge.md.sha1.create());
-    var kem = forge.kem.rsa.create(kdf1);
-    var result = kem.encrypt(keypair.publicKey, 16); //Alex: increase to 256 bit
-    // result has 'encapsulation' and 'key'
-    
-    // encrypt some bytes
-    var iv = forge.random.getBytesSync(12);
-    var someBytes = 'hello world!';
-    var cipher = forge.cipher.createCipher('AES-GCM', result.key);
-    cipher.start({iv: iv});
-    cipher.update(forge.util.createBuffer(someBytes));
-    cipher.finish();
-    var encrypted = cipher.output.getBytes();
-    var tag = cipher.mode.tag.getBytes() as any;
-    
-    // send 'encrypted', 'iv', 'tag', and result.encapsulation to recipient
-    
-    // decrypt encapsulated 16-byte secret key
-    var kdf1 = new forge.kem.kdf1(forge.md.sha1.create());
-    var kem = forge.kem.rsa.create(kdf1);
-    var key = kem.decrypt(keypair.privateKey, result.encapsulation, 16);
-    
-    // decrypt some bytes
-    var decipher = forge.cipher.createDecipher('AES-GCM', key);
-    decipher.start({iv: iv, tag: tag});
-    decipher.update(forge.util.createBuffer(encrypted));
-    var pass = decipher.finish();
-    // pass is false if there was a failure (eg: authentication tag didn't match)
-    if(pass) {
-      // outputs 'hello world!'
-      console.log(decipher.output.getBytes());
-    }
-  }
-
   predict = async(img2) =>  {
-    this.encrypt()
-
     const img = tf.cast(tf.image.resizeBilinear(tf.browser.fromPixels(img2), [this.IMAGE_SIZE, this.IMAGE_SIZE]), 'float32');
     const batch = tf.expandDims(img, 0);
     var score = this._model.predict(batch).dataSync();
